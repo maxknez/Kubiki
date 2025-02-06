f655e09ec77008b6892f6067fe64b4016c6d1f46
maxknez <41018858+maxknez@users.noreply.github.com>
2025-02-06 18:27:31 +0300

    эффект красного экрана от попадания

--- Assets/HitByBullet.cs
+++ Assets/HitByBullet.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
+using System.Collections;
 
 public class HitByBullet : MonoBehaviour
 {
+    [SerializeField] private UnityEngine.Rendering.Volume volume;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Bullet")
         {
             Debug.Log("Hit:");
+            StartCoroutine(ExampleCoroutine());
+
         }
 
     }
+    
+    IEnumerator ExampleCoroutine()
+    {
+            volume.weight = 1;
+            yield return new WaitForSeconds(0.15f);
+            volume.weight = 0;
+    }
+
 }
